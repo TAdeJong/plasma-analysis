5f45c9109271a7bbe4ba52732b4632edfaa7dbba
David Kok <daviduitdelft@gmail.com>
2016-05-17 16:57:45 -0600

    Started working on the c++-code to use mmap to read the vector fields from file

--- code/readin.cpp
+++ code/readin.cpp
@@ -1,9 +1,31 @@
 #include <sys/mman.h>
 #include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <fstream>
 
 using namespace std;
 
-int main() {
-	cout << "Hello world!"<< endl;
+int main(int argc, char** argv) {
+	
+	ofstream floattest;
+	floattest.open(argv[1]);
+	float datapoint = 0.001;
+	floattest << datapoint;
+	floattest.close();
+
+	int numberoffloats = 1;
+	int filedescriptor = open(argv[1], O_RDONLY, 0);
+	
+	float* Data = (float*)mmap(NULL, numberoffloats*sizeof(float), PROT_READ, MAP_FILE | MAP_PRIVATE, filedescriptor, 0);
+	assert(Data != MAP_FAILED);
+
+	for (int i = 0; i < numberoffloats; i++) {
+		cout << Data[i] << " ";
+	}
+	cout << endl;
+
 	return 0;
 }
