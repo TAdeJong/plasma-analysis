1cd57fe860740a340da7990b0d1a571bd3dabf67
David Kok <daviduitdelft@gmail.com>
2016-06-15 10:56:17 +0200

    Removed some prints that were used for debugging, added a check on the start location

--- code/RK4test.cu
+++ code/RK4test.cu
@@ -50,7 +50,7 @@ __device__ float4 RK4step(float4 loc, double dt ) {
 }
 
 __global__ void RK4line(float4* lineoutput, double dt, unsigned int steps, float4 loc) {
-	lineoutput[0] = tex3D(dataTex, make_float3(127.5,127.5,0));
+	lineoutput[0] = tex3D(dataTex, Smiet2Tex(loc));
 	for (unsigned int i=1; i < steps; i++) {
 		loc = loc + RK4step(loc,dt);
 		lineoutput[i] = loc;
@@ -84,7 +84,7 @@ void datagen (float4*** data) {
 
 int main(void) {
 	cudaArray* dataArray;
-	std::cout << 1 << std::endl;
+//	std::cout << 1 << std::endl;
 	cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(32,32,32,32,cudaChannelFormatKindFloat);
 	cudaExtent extent = make_cudaExtent(N , N, N);
 	checkCudaErrors(cudaMalloc3DArray(&dataArray, &channelDesc,extent));
@@ -96,7 +96,7 @@ int main(void) {
 	hostvfield = (float4***) malloc(N*sizeof(float4**));
 	hostvfield[0] = (float4**) malloc(N*N*sizeof(float4*));
 	hostvfield[0][0] = (float4*) malloc(N*N*N*sizeof(float4));
-	std::cout << 2 << std::endl;
+//	std::cout << 2 << std::endl;
 	for (int i=1; i < N; i++) {
 		hostvfield[i] = (hostvfield[0] + i*N);
 	}
@@ -105,18 +105,18 @@ int main(void) {
 			hostvfield[i][j] = (hostvfield[0][0] + (i*N*N + j*N));
 		}
 	}
-	std::cout << 2 << std::endl;
+//	std::cout << 2 << std::endl;
 	datagen(hostvfield);
-	std::cout << extent.width << extent.height << extent.depth << std::endl;
+//	std::cout << extent.width << extent.height << extent.depth << std::endl;
 //copy to device
 	cudaMemcpy3DParms copyParms = {0};
 	copyParms.srcPtr = make_cudaPitchedPtr((void *)hostvfield[0][0], extent.width* sizeof(float4), extent.height, extent.depth);
-	std::cout << 4 << std::endl;
+//	std::cout << 4 << std::endl;
 	copyParms.dstArray = dataArray;
 	copyParms.extent = extent;
-	copyParms.kind     = cudaMemcpyHostToDevice;
+	copyParms.kind = cudaMemcpyHostToDevice;
 	checkCudaErrors(cudaMemcpy3D(&copyParms));
-	std::cout << 5 << std::endl;
+//	std::cout << 5 << std::endl;
 	checkCudaErrors(cudaBindTextureToArray(dataTex, dataArray, channelDesc));
 	float4 *d_lines, *h_lines;
 	double time = 3.141592653*2.0;
@@ -125,13 +125,15 @@ int main(void) {
 	int blocks = 1;
 	float dt = time/N;
 	float4 startloc = {0,0,0,0};
-	std::cout << hostvfield[127][127][127].x << std::endl;
+	float3 locSmiet = Smiet2Tex(startloc);
+	std::cout << "Starting point in texture coordinates: x=" << locSmiet.x << ", y=" << locSmiet.y << ", z=" << locSmiet.z << std::endl;
+//	std::cout << hostvfield[127][127][127].x << std::endl;
 	checkCudaErrors(cudaMalloc(&d_lines,blocks*cores*steps*sizeof(float4)));
 
 	h_lines = (float4*) malloc(blocks*cores*steps*sizeof(float4));
 	RK4line<<<cores,blocks>>>(d_lines, dt, steps, startloc);
 	checkCudaErrors(cudaMemcpy(h_lines, d_lines, blocks*cores*steps*sizeof(float4), cudaMemcpyDeviceToHost));
-	for(unsigned int i=0; i<steps; i++) {
+	for(unsigned int i=0; i<3; i++) {
 		std::cout << "x= " << h_lines[i].x << "; y= "<< h_lines[i].y << " "<< h_lines[i].x*h_lines[i].x+h_lines[i].y*h_lines[i].y << std::endl;
 	}
 	free(hostvfield[0][0]);
