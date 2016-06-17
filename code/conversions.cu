edf4166f7b5193a60468ec9369cfe4b2e0b9d6c7
T.A. de Jong <tadj42@gmail.com>
2016-06-17 16:48:37 +0200

    Wrote functions to do reductions properly
    Used for origin vector finding
    
    a sum calculation and a specific crossproduct calculation
    based on:
    http://developer.download.nvidia.com/compute/cuda/1.1-Beta/x86_website/projects/reduction/doc/reduction.pdf

--- code/conversions.cu
+++ code/conversions.cu
@@ -1,7 +1,7 @@
 #include "coordfunctions.h"
 #include "constants.h"
 #include "conversions.h"
-
+#include "helper_math.h"
 /*	Uses parallel computing to determine the origin (middle) of each of the field lines
 	computed with RK4 and stored in lineoutput earlier.
 	Warning: only works when the total number of threads used to call this function
@@ -32,6 +32,38 @@ __device__ float4 origin(float4* lineoutput, int steps, dim3 gridsize, int numbe
 	return origin;
 }
 
+__device__ void reducesum(float4* g_linedata, float4* g_sumdata) {
+	extern __shared__ float4 sdata[];
+
+	//load data from global data to shared mem and perform first reduction
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x*blockDim.x*2 + threadIdx.x;
+	sdata[i] = g_linedata[i]+g_linedata[i+blockDim.x];
+	__syncthreads();
+
+	//do the reductions
+	for( unsigned int s=blockDim.x/2; s>32; s>>=1) {//32 = warpsize
+		if(tid < s) {
+			sdata[tid] += sdata[tid+s];
+		}
+
+		__syncthreads();
+	}
+	if(tid<32) {// Warp's zijn SIMD gesynchroniseerd
+		sdata[tid] += sdata[tid + 32];
+		sdata[tid] += sdata[tid + 16];
+		sdata[tid] += sdata[tid + 8];
+		sdata[tid] += sdata[tid + 4];
+		sdata[tid] += sdata[tid + 2];
+		sdata[tid] += sdata[tid + 1];
+	}
+	//write result to global
+	if(tid == 0) g_sumdata[blockIdx.x] = sdata[0];
+}
+
+
+
+
 __device__ float4 normal(float4* lineoutput, int steps, dim3 gridsize, int numberoflines, float4* communication, float4 origin) {
 	float4 normal = make_float4(0.0)
 	dim3 index2D(threadIdx.x + blockIdx.x * blockDim.x, threadIdx.y + blockIdx.y*blockDim.y);
@@ -55,4 +87,33 @@ __device__ float4 normal(float4* lineoutput, int steps, dim3 gridsize, int numbe
 		
 	}
 	return normal;
+}
+
+__device__ void reducenormal(float4* g_linedata, float4* g_normaldata) {//equivalent to doing the texture-fetch and cross product and applying reducesum
+	extern __shared__ float4 sdata[];
+
+	//load data from global data&texture to shared mem and perform cross product
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+	sdata[i] = cross(make_float_3(g_linedata[i]), tex3D(Smiet2Tex(g_linedata[i])));
+	__syncthreads();
+
+	//do the reductions
+	for( unsigned int s=blockDim.x; s>32; s>>=1) {//32 = warpsize
+		if(tid < s) {
+			sdata[tid] += sdata[tid+s];
+		}
+
+		__syncthreads();
+	}
+	if(tid<32) {// Warp's zijn SIMD gesynchroniseerd
+		sdata[tid] += sdata[tid + 32];
+		sdata[tid] += sdata[tid + 16];
+		sdata[tid] += sdata[tid + 8];
+		sdata[tid] += sdata[tid + 4];
+		sdata[tid] += sdata[tid + 2];
+		sdata[tid] += sdata[tid + 1];
+	}
+	//write result to global
+	if(tid == 0) g_sumdata[blockIdx.x] = sdata[0];
 }
--- code/conversions.h
+++ code/conversions.h
@@ -5,8 +5,11 @@
 #ifndef _CONVERSIONS_H_
 #define _CONVERSIONS_H_
 
-__device__ float4 origin(float4* lineoutput, int steps, dim3 gridsize, int numberoflines, float4* communication)
+__device__ float4 origin(float4* lineoutput, int steps, dim3 gridsize, int numberoflines, float4* communication);
 
-__device__ float4 normal(float4* lineoutput, int steps, dim3 gridsize, int numberoflines, float4* communication, float4 origin)
+__device__ float4 normal(float4* lineoutput, int steps, dim3 gridsize, int numberoflines, float4* communication, float4 origin);
 
+__device__ void reducesum( float4* g_linedata, float4* g_sumdata);
+
+__device__ void reducenormal( float4* g_linedata, float4* g_normaldata);
 #endif
