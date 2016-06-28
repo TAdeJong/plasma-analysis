30ff679a9e04d481f3626db2b07409c720912d13
T.A. de Jong <tadj42@gmail.com>
2016-06-28 23:25:18 +0200

    Implemented a function for pioncare
    It counts the number of x=0 transitions

--- code/conversions.cu
+++ code/conversions.cu
@@ -12,6 +12,38 @@ extern texture <float4, cudaTextureType3D, cudaReadModeElementType> dataTex;
 	(so each RK4 line will be processed within a single block)
 */
 
+__device__ int signdiff(float a, float b) {
+	return (a < 0 && b >= 0) || (a>0 && b <=0);
+}
+
+//find the number of x=0 transitions in g_linedata, storing the result in g_sumdata. Only works for powers of 2 datasets and needs a minimum of sdata of 64*sizeof(float) (!)
+__global__ void reducePC(float4* g_linedata, float4* g_PCdata) {
+	extern __shared__ int sdata[];
+	//load data from global data&texture to shared mem and perform cross product
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+	sdata[tid] = signdiff(g_linedata[i].x,g_linedata[i+1].x);
+	__syncthreads();
+
+	//do the reductions
+	for( unsigned int s=blockDim.x/2; s>32; s>>=1) {//32 = warpsize
+		if(tid < s) {
+			sdata[tid] += sdata[tid+s];
+		}
+
+		__syncthreads();
+	}
+	if(tid<32) {// Warp's zijn SIMD gesynchroniseerd
+		sdata[tid] += sdata[tid + 32];
+		sdata[tid] += sdata[tid + 16];
+		sdata[tid] += sdata[tid + 8];
+		sdata[tid] += sdata[tid + 4];
+		sdata[tid] += sdata[tid + 2];
+		sdata[tid] += sdata[tid + 1];
+	}
+	//write result to global
+	if(tid == 0) g_normaldata[blockIdx.x] = sdata[0];
+}
 
 //Sum all elements in g_linedata, storing the result in g_sumdata. Only works for powers of 2 datasets and needs a minimum of sdata of 64*sizeof(float4) (!)
 __global__ void reduceSum(float4* g_linedata, float4* g_sumdata) {
