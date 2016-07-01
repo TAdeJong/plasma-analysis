8ee74a19a5b6b4d37300c1d8c8d710a88983d148
T.A. de Jong <tadj42@gmail.com>
2016-07-01 13:37:09 +0200

    Bugfixes on reducePC

--- code/conversions.cu
+++ code/conversions.cu
@@ -17,32 +17,32 @@ __device__ int signdiff(float a, float b) {
 }
 
 //find the number of x=0 transitions in g_linedata, storing the result in g_sumdata. Only works for powers of 2 datasets and needs a minimum of sdata of 64*sizeof(float) (!)
-__global__ void reducePC(float4* g_linedata, float4* g_PCdata) {
-	extern __shared__ int sdata[];
+__global__ void reducePC(float4* g_linedata, int* g_PCdata) {
+	extern __shared__ int idata[];
 	//load data from global data&texture to shared mem and perform cross product
 	unsigned int tid = threadIdx.x;
 	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-	sdata[tid] = signdiff(g_linedata[i].x,g_linedata[i+1].x);
+	idata[tid] = signdiff(g_linedata[i].x,g_linedata[i+1].x);
 	__syncthreads();
 
 	//do the reductions
 	for( unsigned int s=blockDim.x/2; s>32; s>>=1) {//32 = warpsize
 		if(tid < s) {
-			sdata[tid] += sdata[tid+s];
+			idata[tid] += idata[tid+s];
 		}
 
 		__syncthreads();
 	}
 	if(tid<32) {// Warp's zijn SIMD gesynchroniseerd
-		sdata[tid] += sdata[tid + 32];
-		sdata[tid] += sdata[tid + 16];
-		sdata[tid] += sdata[tid + 8];
-		sdata[tid] += sdata[tid + 4];
-		sdata[tid] += sdata[tid + 2];
-		sdata[tid] += sdata[tid + 1];
+		idata[tid] += idata[tid + 32];
+		idata[tid] += idata[tid + 16];
+		idata[tid] += idata[tid + 8];
+		idata[tid] += idata[tid + 4];
+		idata[tid] += idata[tid + 2];
+		idata[tid] += idata[tid + 1];
 	}
 	//write result to global
-	if(tid == 0) g_normaldata[blockIdx.x] = sdata[0];
+	if(tid == 0) g_PCdata[blockIdx.x] = idata[0];
 }
 
 //Sum all elements in g_linedata, storing the result in g_sumdata. Only works for powers of 2 datasets and needs a minimum of sdata of 64*sizeof(float4) (!)
