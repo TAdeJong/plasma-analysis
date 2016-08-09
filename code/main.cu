b314863cb173bb55c4679537677523a452057e2f
T.A. de Jong <tadj42@gmail.com>
2016-08-09 11:22:23 +0200

    Debug function for linelength output
    Correct usage of reduceSum

--- code/main.cu
+++ code/main.cu
@@ -93,9 +93,9 @@ int main(int argc, char *argv[]) {
 	int steps = 8*blockSize;
 	float dt = 1/8.0;
 
-	dim3 gridsizeRK4(1,1);
-	dim3 blocksizeRK4(8,8);
-	int dataCount = gridsizeRK4.x*gridsizeRK4.y*blocksizeRK4.x*blocksizeRK4.y*steps;
+	dim3 gridSizeRK4(1,1);
+	dim3 blockSizeRK4(8,8);
+	int dataCount = gridSizeRK4.x*gridSizeRK4.y*blockSizeRK4.x*blockSizeRK4.y*steps;
 	float4 startloc = make_float4(-1,0,0,0); //Location (in Smietcoords) to start the integration, to be varied
 	float4 xvec = {1,0,0,0};
 	float4 yvec = {0,1,0,0};
@@ -107,7 +107,7 @@ int main(int argc, char *argv[]) {
 	h_lines = (float4*) malloc(dataCount*sizeof(float4));
 
 	//Integrate the vector field
-	RK4line<<<gridsizeRK4,blocksizeRK4>>>(d_lines, dt, steps, startloc, xvec, yvec, gridsizeRK4);
+	RK4line<<<gridSizeRK4,blockSizeRK4>>>(d_lines, dt, steps, startloc, xvec, yvec, gridSizeRK4);
 
 	float4 *d_origins;
 	checkCudaErrors(cudaMalloc(&d_origins, dataCount/(2*blockSize)*sizeof(float4)));
@@ -147,11 +147,22 @@ int main(int argc, char *argv[]) {
 
 	//Add the length of the pieces of the lines to obtain line length
 	//Stores the length of the i'th line in d_lengths[i]
-	reduceSum<<<dataCount/steps,steps/2,(steps/2)*sizeof(float)>>>(d_lengths,d_lengths);
+	reduceSum<<<dataCount/(2*blockSize),blockSize,blockSize*sizeof(float)>>>(d_lengths,d_lengths);
+	reduceSum<<<dataCount/steps,steps/(4*blockSize),steps/(4*blockSize)*sizeof(float)>>>(d_lengths,d_lengths);
+
 
 	//Copy lengths from device to host
 	checkCudaErrors(cudaMemcpy(h_lengths, d_lengths, (dataCount/steps)*sizeof(float), cudaMemcpyDeviceToHost));
 	
+
+	for(unsigned int i=0; i< blockSizeRK4.x; i++) {
+		for(unsigned int j=0; j< blockSizeRK4.y; j++) {
+			std::cout << h_lengths[blockSizeRK4.y*i+j] << " , ";
+		}
+		std::cout << std::endl;
+	}
+		
+
 	//Write all the lines
 	datawrite("../datadir/data.bin", dataCount, h_lines);
    
