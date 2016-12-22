4e5067651f9fe91a491e6dc7f04e97816d71b3d7
smiet <smiet@physics.leidenuniv.nl>
2016-12-22 14:26:08 +0100

    Tweaks in plotfunction datafuse2.py
    IMprovements on the windingvstime plot

--- code/datafuse2.py
+++ code/datafuse2.py
@@ -13,7 +13,7 @@ elif (len(sys.argv) > 2) :
 	fuz = float(sys.argv[2])
 binfile = sys.argv[1]
 lengthdata=np.fromfile(binfile+'_lengths.bin', dtype="float32")
-minLength = 300.0
+minLength = 100.0
 winddata=np.fromfile(binfile+'_windings.bin', dtype="float32")
 datasize = int(np.sqrt(lengthdata.shape[0]))
 lengthdata=lengthdata.reshape(datasize, datasize)
@@ -22,10 +22,18 @@ masked= ma.masked_where(lengthdata<minLength,winddata)
 clampVal = np.mean(masked)
 dev = ma.std(masked)
 fractions = sorted([-1./3,-2./3,-1./5,-2./5,-3./5,-4./5,-1./4,-1./2,-3./4,-2./7,-3./7,-4./7,-5./7,-6./7,-3./8,-5./8,-7./8])
-print(fractions)
+#print(fractions)
+fig = plt.figure()
 cmap = plt.cm.get_cmap('nipy_spectral',8192)
-img = plt.imshow(masked.filled(0), clim=[-0.667,-0.595], cmap=cmap)
+img = plt.imshow(masked.filled(0), 
+        clim=[-5,-2.5], 
+        cmap=cmap)
 #img.set_cmap('nipy_spectral')
-plt.colorbar(img, ticks = fractions)
-plt.show()
+plt.colorbar(img,
+#        ticks = fractions
+        )
+#plt.show()
 # plt.savefig(sys.argv[1].rsplit(".",1)[0]+'_fig.png')
+sizes = fig.get_size_inches()
+fig.set_size_inches(sizes[0]*2,sizes[1]*2)
+plt.savefig('tobiastestdingen.png')
--- code/windingvstime.py
+++ code/windingvstime.py
@@ -14,9 +14,9 @@ elif (len(sys.argv) > 2) :
 	fuz = float(sys.argv[2])
 binfile = sys.argv[1]
 linedata = []
-for i in np.linspace(140,33,15) :
+for i in np.linspace(290,0,15) :
     lengthdata=np.fromfile(binfile+str(int(i))+'_lengths.bin', dtype="float32")
-    minLength = 500.0
+    minLength = 200.0
     winddata=np.fromfile(binfile+str(int(i))+'_windings.bin', dtype="float32")
     datasize = int(np.sqrt(lengthdata.shape[0]))
     lengthdata=lengthdata.reshape(datasize, datasize)
@@ -31,10 +31,13 @@ cmap = matplotlib.cm.get_cmap('jet')
 #Wist je dat dit ook zonder forloop kan?
 for dataset,i in linedata :
     x = np.linspace(0,np.pi,datasize)
-    ax.plot(x,dataset,'.',label='t='+i, color=cmap(int(i)/140.))
-ax.set_ylim([-1,-0.5])
-ax.set_xlim([x[100],x[800]])
+    ax.plot(x,-1.*dataset,'.',label='t='+i, color=cmap(int(i)/240.))
+ax.set_ylim([0.5,9])
+#ax.set_xlim([x[0],x[800]])
 ax.set_ylabel('Winding number')
 ax.set_xlabel(r'$\sim r$')
-plt.legend(loc='right')
-plt.show()
+#plt.colorbar()
+plt.legend(loc=1)
+sizes = fig.get_size_inches()
+fig.set_size_inches(sizes[0]*2,sizes[1]*2)
+plt.savefig('tobiastestdingen.png')
